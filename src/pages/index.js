e1f8cfc04890b22ce536a2b9dc3db6c5ed3c4304
Lench Volodymyr <volodymyr.lench@binary-studio.com>
2021-11-22 18:52:33 +0200

    add avatar for lench volodymyr

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,8 @@ import {
   pavelNemchenko2017Avatar,
   rostyslavDiakiv2018Avatar,
   nikitaKrasnov2018Avatar,
-  kyryloLesohorskyi2020Avatar
+  kyryloLesohorskyi2020Avatar,
+  LenchVolodymyr2021Avatar
  } from "../assets/images/avatars/intro-rounded";
 import "./index.css";
 
@@ -25,7 +26,7 @@ const getAuthorAvatar = author => {
     case "Павел Немченко":
       return pavelNemchenko2017Avatar;
     case "Владимир Ленч":
-      return pavelNemchenko2017Avatar;
+      return LenchVolodymyr2021Avatar;
     case "Ростислав Дяків":
       return rostyslavDiakiv2018Avatar;
     case "Никита Краснов":
